e053128a87bc6d895f195e42194b3c256e8697bd
Surin Lee <leesurin99@gmail.com>
2022-03-30 18:59:38 +0900

    update gitignore

--- django_prj/urls.py
+++ django_prj/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.views.static import serve
 from django.contrib import admin
 from django.urls import path, include, re_path
 from drf_yasg import openapi
@@ -20,6 +21,7 @@ from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
 from cafe import views
+from django_prj import settings
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -39,6 +41,7 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('admin', admin.site.urls),
     path('', include('cafe.urls')),
     path('login', views.LoginView.as_view(), name='login'),
